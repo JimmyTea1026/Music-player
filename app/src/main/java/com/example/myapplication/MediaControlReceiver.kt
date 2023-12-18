d99bbffb4ed4b45dee27ea48ce059e6c1deb57f3
JimmyTea1026 <jimmy871026@gmail.com>
2023-12-18 15:26:53 +0800

    尚未完成通知列

---
+++ app/src/main/java/com/example/myapplication/MediaControlReceiver.kt
@@ -0,0 +1,23 @@
+package com.example.myapplication
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.util.Log
+import com.example.myapplication.PlayPage.PlayPageViewModel
+
+class MediaControlReceiver : BroadcastReceiver() {
+    override fun onReceive(context: Context?, intent: Intent?) {
+        val action = intent?.action
+        if (action == "PLAY_PAUSE_ACTION") {
+            Log.d("", "ss")
+            if(PlayPageViewModel.mediaPlayer.isPlaying) PlayPageViewModel.mediaPlayerPause()
+            else PlayPageViewModel.mediaPlayerStart()
+        } else if (action == "NEXT_ACTION") {
+            PlayPageViewModel.setSong(1)
+        } else if (action == "PREVIOUS_ACTION") {
+            PlayPageViewModel.setSong(-1)
+        }
+        // 可以根據需要處理更多的操作
+    }
+}
